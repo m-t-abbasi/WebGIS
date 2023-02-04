85d647a0be4900ae0e910f5ad8a99a837b28779f
mohammadtaghi <110107060+m-t-abbasi@users.noreply.github.com>
2023-02-04 12:58:15 +0330

    Delete login.js

--- login.js
+++
@@ -1,5 +0,0 @@
-const username = document.getElementById("username").firstChild;
-username.placeholder = "Your username...";
-
-const password = document.getElementById("password").firstChild;
-password.placeholder = "Your password...";
