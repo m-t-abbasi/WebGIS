9826c6280a39bee4c73eab5ad6e6034f24296af4
mohammadtaghi <110107060+m-t-abbasi@users.noreply.github.com>
2023-02-08 18:22:22 +0330

    Delete webgis_project directory

--- webgis_project/map/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_page, name='login_page'),
-    path('map/', views.map, name='map'),
-    path('logout/', views.logout_page, name='logout_page'),
-]
